42db8b159bd136a0b796e80b8f58e3f93fde6594
Chris Wilson <chris@chrisbwilson.com>
2021-01-01 16:36:37 -0800

    Initial Commit

---
+++ SwiftCrestronCIP/SwiftCrestronCIP.h
@@ -0,0 +1,18 @@
+//
+//  SwiftCrestronCIP.h
+//  SwiftCrestronCIP
+//
+//  Created by Chris Wilson on 1/1/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftCrestronCIP.
+FOUNDATION_EXPORT double SwiftCrestronCIPVersionNumber;
+
+//! Project version string for SwiftCrestronCIP.
+FOUNDATION_EXPORT const unsigned char SwiftCrestronCIPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCrestronCIP/PublicHeader.h>
+
+
